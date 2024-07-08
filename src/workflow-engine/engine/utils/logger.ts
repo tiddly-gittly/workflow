b125380ccd8b95d7bc28f01c91335892c1aeb7e8
lin onetwo <linonetwo012@gmail.com>
2024-07-09 01:44:21 +0800

    fix: missing log method

--- src/workflow-engine/engine/utils/logger.ts
+++ src/workflow-engine/engine/utils/logger.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/strict-boolean-expressions */
 import { Logger } from '$:/core/modules/utils/logger.js';
 import { ILogger } from 'bpmn-server';
 
@@ -5,14 +6,14 @@ export class TwEngineLogger implements ILogger {
   private toConsole: boolean = true;
   private toFile: boolean = false;
   private callback: CallableFunction | undefined = undefined;
-  private messages: any[] = [];
+  private messages: Array<{ level: string; message: string[][] | Error; stack?: string }> = [];
   private readonly twLogger?: Logger;
   constructor() {
     this.setOptions({ toConsole: true, toFile: false, callback: undefined });
     this.twLogger = new $tw.utils.Logger('tw-datastore');
   }
 
-  setOptions({ toConsole, toFile, callback }: { callback: any; toConsole: boolean; toFile: boolean }): void {
+  setOptions({ toConsole, toFile, callback }: { callback?: CallableFunction; toConsole: boolean; toFile: boolean }): void {
     this.toConsole = toConsole;
     this.toFile = toFile;
     this.callback = callback;
@@ -22,46 +23,58 @@ export class TwEngineLogger implements ILogger {
     this.messages = [];
   }
 
-  get(): any[] {
+  get() {
     return this.messages;
   }
 
-  debug(...message: any): void {
+  debug(...message: string[][]): void {
     this.messages.push({ level: 'debug', message });
     if (this.toConsole) {
       this.twLogger?.log?.(...message);
     }
   }
 
-  warn(...message: any): void {
+  warn(...message: string[][]): void {
     this.messages.push({ level: 'warn', message });
     if (this.toConsole) {
       this.twLogger?.log?.(...message);
     }
   }
 
-  log(...message: any): void {
+  log(...message: string[][]): void {
     this.messages.push({ level: 'log', message });
     if (this.toConsole) {
       this.twLogger?.log?.(...message);
     }
   }
 
+  info(...message: string[][]): void {
+    this.log(...message);
+  }
+
+  logS(...message: string[][]): void {
+    this.info(...message);
+  }
+
   error(error: Error): void {
     this.messages.push({ level: 'error', message: error, stack: error.stack });
     if (this.toConsole) {
-      this.twLogger?.alert?.(error);
+      this.twLogger?.alert?.('error occurred', error, error.stack);
     }
   }
 
-  reportError(error: any): void {
+  logE(error: Error): void {
+    this.error(error);
+  }
+
+  reportError(error: Error): void {
     this.error(error);
     if (this.callback) {
       this.callback(error);
     }
   }
 
-  async save(filename: any): Promise<void> {
+  async save(filename: string): Promise<void> {
     // not implemented
   }
 }
